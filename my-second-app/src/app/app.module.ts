a2a3ac3a2c894d4018cad200c88981e8a495a879
HH22OO <nicolas.riopel.1@ens.etsmtl.ca>
2022-05-11 16:29:45 -0400

    2nd commit

--- my-second-app/src/app/app.module.ts
+++ my-second-app/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { FormsModule } from '@angular/forms';
+import { DropdownDirective } from './shared/dropdown.directive';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 @NgModule({
   declarations: [
@@ -21,9 +23,10 @@ import { FormsModule } from '@angular/forms';
     ShoppingListComponent,
     ShoppingEditComponent,
     RecipeDetailComponent,
+    DropdownDirective,
   ],
   imports: [FormsModule, BrowserModule],
-  providers: [],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
